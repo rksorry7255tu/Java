a831d7bfdcaf5938bdb8398dd2b6bf28323881ed
rksorry7255tu <rk7255053708@gmail.com>
2025-01-23 16:48:46 +0530

    java basic

---
+++ javabasic/FirstClass.java
@@ -0,0 +1,13 @@
+public class FirstClass {
+  public static void main(String args[]) {
+    System.out.println("hello world");
+    System.out.print("Ravi\n");
+    System.out.println("Ravi");
+    System.out.println("*\n**\n***\n****");
+    String name="Ravi";
+    int a=25;
+    int b=10;
+    int age=20;
+    
+  }
+}
