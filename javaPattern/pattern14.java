e7f1cc04b85e2950c1558dc396e063f8d97e4be7
rksorry7255tu <rk7255053708@gmail.com>
2025-09-25 00:02:17 +0530

    commited

--- javaPattern/pattern14.java
+++ javaPattern/pattern14.java
@@ -3,7 +3,14 @@ import java.util.*;
 public class pattern14 {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
+    int A = 65;
     int n = sc.nextInt();
-    
+    for (int i = 0; i < n; i++) {
+      int q = A;
+      for (int j = 0; j < n - i; j++) {
+        System.out.print((char) q++);
+      }
+      System.out.println();
+    }
   }
 }
