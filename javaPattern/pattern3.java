176898d8f2b81fd9c7a38e56c2b730d6aca74f8f
rksorry7255tu <rk7255053708@gmail.com>
2025-09-24 22:41:01 +0530

    commited

---
+++ javaPattern/pattern3.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class pattern3 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    for (int i = 0; i < n; i++) {
+      for (int j = 1; j < i + 2; j++) {
+        System.out.print(j);
+      }
+      System.out.println();
+    }
+  }
+}
---
+++ javaPattern/pattern5.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class pattern5 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n - 1 - i; j++) {
+        System.out.print(" ");
+      }
+      for (int j = 0; j < (i * 2) + 1; j++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+}
