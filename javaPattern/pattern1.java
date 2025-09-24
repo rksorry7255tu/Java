654a31a71501f60a3277e632f3e558aa9b59bf32
rksorry7255tu <rk7255053708@gmail.com>
2025-09-24 22:17:07 +0530

    commited

--- javaPattern/pattern1.java
+++ javaPattern/pattern1.java
@@ -1,5 +1,15 @@
+import java.util.*;
+
 public class pattern1 {
   public static void main(String[] args) {
-    
+    System.out.println("Enter the no of row value");
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
   }
 }
