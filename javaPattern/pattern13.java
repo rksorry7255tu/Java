48ffd98a046531a7522b7391b91837643cc69de5
rksorry7255tu <rk7255053708@gmail.com>
2025-09-24 23:58:45 +0530

    commited

---
+++ javaPattern/pattern13.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class pattern13 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int A = 65;
+    for (int i = 0; i < n; i++) {
+      int q = A;
+      for (int j = 0; j < i + 1; j++) {
+        System.out.print((char) q++);
+      }
+      System.out.println();
+    }
+  }
+
+}
