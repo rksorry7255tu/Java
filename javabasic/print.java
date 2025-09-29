c0518febd3e344ab91b4fba6b06cf4626a4f2d27
rksorry7255tu <rk7255053708@gmail.com>
2025-09-29 22:24:46 +0530

    commited

---
+++ javabasic/print.java
@@ -0,0 +1,18 @@
+public class print {
+  public static void main(String[] args) {
+    // int arr[][] = {
+    // { 1, 2, 3 },
+    // { 4, 5 },
+    // { 7, 8, 9 }
+    // };
+    // System.out.println(arr[0][0]);
+    // System.out.println(arr[1][0]);
+    // System.out.println(arr[2][0]);
+
+    int arr[][] = new int[3][];
+    arr[0] = new int[2];
+    arr[1] = new int[3];
+    arr[2] = new int[4];
+    
+  }
+}
