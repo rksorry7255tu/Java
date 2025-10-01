2fa12d4b686339a06b80524a039362ba94dbf6b7
rksorry7255tu <rk7255053708@gmail.com>
2025-10-01 23:34:24 +0530

    commited

---
+++ javaCollectionsFramework/list/list1.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class list1 {
+  public static void main(String[] args) {
+    ArrayList<Integer> list = new ArrayList<>();
+    list.add(1);
+    list.add(2);
+  }
+}
