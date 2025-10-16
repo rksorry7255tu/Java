4a11a6da70c93cef9623d5be16f0bfd16bdf045e
rksorry7255tu <rk7255053708@gmail.com>
2025-10-17 02:01:50 +0530

    commited

---
+++ Tree/preOrderTree.java
@@ -0,0 +1,39 @@
+
+public class preOrderTree {
+
+  static class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+      this.data = data;
+      this.left = null;
+      this.right = null;
+    }
+  }
+
+  static class BinaryTree {
+    static int idx = -1;
+
+    public static Node buildTree(int nodes[]) {
+      idx++;
+      if (nodes[idx] == -1) {
+        return null;
+      }
+
+      Node newnode = new Node(nodes[idx]);
+      newnode.left = buildTree(nodes);
+      newnode.right = buildTree(nodes);
+      return newnode;
+    }
+  }
+
+  public static void main(String[] args) {
+    int nodes[] = { 1, 2, 3, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+
+    BinaryTree tree = new BinaryTree();
+    Node root = tree.buildTree(nodes);
+    System.out.println(root.data);
+  }
+}
